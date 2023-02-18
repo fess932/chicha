a16e8d0b4d1a79ac3d81fc7faf5c800f7629cc20
Ivan Cheremisin <fess932@gmail.com>
2023-02-18 18:27:09 +0300

    think about refactornig

--- pkg/domain/event.go
+++ pkg/domain/event.go
@@ -1,5 +1,9 @@
 package domain
 
+import "time"
+
 // Event входящее событие из источника данных, rfid ридера например
 type Event struct {
+	Date       time.Time // время события из источника данных
+	IncomeDate time.Time // время поступления события
 }
